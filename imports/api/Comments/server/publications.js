10c1a9277b40f2f09c324835b4bd1a86cd17d6e9
Robert Smith <konokhead@gmail.com>
2017-11-30 08:11:13 -0600

    .

---
+++ imports/api/Comments/server/publications.js
@@ -0,0 +1,27 @@
+import { Meteor } from 'meteor/meteor';
+import { check } from 'meteor/check';
+import { Counts } from 'meteor/tmeasday:publish-counts';
+import Documents from '../Documents';
+import Comments from '../../Comments/Comments';
+
+[...]
+
+// Note: documents.view is also used when editing an existing document.
+Meteor.publish('documents.view', function documentsView(documentId, isEditing) {
+  check(documentId, String);
+  check(isEditing, Match.Maybe(Boolean));
+
+  const query = { _id: documentId };
+  if (isEditing) query.owner = this.userId;
+
+  // Run a separate query for the actual comments vs. the one we use for the count as the counter
+  // modifies the query, omitting any fields other than _id.
+  Counts.publish(this, 'documents.view.commentCount', Comments.find({ documentId }));
+  const comments = Comments.find({ documentId });
+
+  return [
+    Documents.find(query),
+    comments,
+    Meteor.users.find({ _id: { $in: comments.fetch().map(({ author }) => author) } }, { profile: { profile: 1 } }),
+  ];
+});
